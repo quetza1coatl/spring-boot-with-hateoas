ef84b9936b7802cbfed0f05ee7a941299474d9a8
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-09-15 20:38:29 +0300

    init

---
+++ src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/SpringbootCourseWithHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.quetzalcoatl.springbootcoursewithhateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCourseWithHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCourseWithHateoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quetzalcoatl/springbootcoursewithhateoas/SpringbootcoursewithhateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quetzalcoatl.springbootcoursewithhateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcoursewithhateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
