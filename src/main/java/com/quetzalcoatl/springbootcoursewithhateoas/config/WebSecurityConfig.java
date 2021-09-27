92fb084cb9555ef7592886bebf10e2fd0b32112c
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-09-27 21:57:27 +0300

    role and grantedAuthority

--- src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/config/WebSecurityConfig.java
+++ src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.quetzalcoatl.springbootcoursewithhateoas.config;
 
 import com.quetzalcoatl.springbootcoursewithhateoas.AuthUser;
 import com.quetzalcoatl.springbootcoursewithhateoas.model.User;
+import com.quetzalcoatl.springbootcoursewithhateoas.model.Role;
 import com.quetzalcoatl.springbootcoursewithhateoas.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,5 +41,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService())
                 .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/account").hasRole(Role.USER.name())
+                .antMatchers("/api/**").hasRole(Role.ADMIN.name())
+                .and().formLogin();
     }
 }
