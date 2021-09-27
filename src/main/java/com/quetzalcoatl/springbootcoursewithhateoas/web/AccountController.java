a25c4558684dc13320342026223f7d36c96d059e
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-09-27 22:03:09 +0300

    add CRUD operations

--- src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/web/AccountController.java
+++ src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/web/AccountController.java
@@ -1,19 +1,68 @@
 package com.quetzalcoatl.springbootcoursewithhateoas.web;
 
 import com.quetzalcoatl.springbootcoursewithhateoas.AuthUser;
+import com.quetzalcoatl.springbootcoursewithhateoas.model.Role;
 import com.quetzalcoatl.springbootcoursewithhateoas.model.User;
+import com.quetzalcoatl.springbootcoursewithhateoas.repository.UserRepository;
+import com.quetzalcoatl.springbootcoursewithhateoas.util.ValidationUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashSet;
 
 @RestController
 @RequestMapping(value = "/api/account")
+@AllArgsConstructor
+@Slf4j
 public class AccountController {
+
+    private final UserRepository userRepository;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get(@AuthenticationPrincipal AuthUser authUser) {
+        log.info("get {}", authUser);
         return authUser.getUser();
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@AuthenticationPrincipal AuthUser authUser) {
+        log.info("delete {}", authUser);
+        userRepository.deleteById(authUser.id());
+    }
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public ResponseEntity<User> register(@Valid @RequestBody User user) {
+        log.info("register {}", user);
+        ValidationUtil.checkNew(user);
+        user.setRoles(new HashSet<>(Collections.singletonList(Role.USER)));
+        user = userRepository.save(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/account")
+                .build().toUri();
+        return ResponseEntity.created(uriOfNewResource).body(user);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@Valid @RequestBody User user, @AuthenticationPrincipal AuthUser authUser) {
+        log.info("update {} to {}", authUser, user);
+        User oldUser = authUser.getUser();
+        ValidationUtil.assureIdConsistent(user, oldUser.id());
+        user.setRoles(oldUser.getRoles());
+        if (user.getPassword() == null) {
+            user.setPassword(oldUser.getPassword());
+        }
+        userRepository.save(user);
     }
 }
