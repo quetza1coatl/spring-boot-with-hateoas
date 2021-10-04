842c4f817f796d1dfbd6dff41594464825fe90ec
Quetzalcoatl <www.quetzalcoatl@gmail.com>
2021-10-04 21:06:48 +0300

    error handling

--- src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/util/ValidationUtil.java
+++ src/main/java/com/quetzalcoatl/springbootcoursewithhateoas/util/ValidationUtil.java
@@ -1,12 +1,13 @@
 package com.quetzalcoatl.springbootcoursewithhateoas.util;
 
+import com.quetzalcoatl.springbootcoursewithhateoas.error.IllegalRequestDataException;
 import com.quetzalcoatl.springbootcoursewithhateoas.model.BaseEntity;
 
 public class ValidationUtil {
 
     public static void checkNew(BaseEntity entity) {
         if (!entity.isNew()) {
-            throw new IllegalArgumentException(entity + " must be new (id=null)");
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must be new (id=null)");
         }
     }
 
@@ -15,7 +16,7 @@ public class ValidationUtil {
         if (entity.isNew()) {
             entity.setId(id);
         } else if (entity.id() != id) {
-            throw new IllegalArgumentException(entity + " must has id=" + id);
+            throw new IllegalRequestDataException(entity.getClass().getSimpleName() + " must has id=" + id);
         }
     }
 }
